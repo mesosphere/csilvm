145b9a749ad1c2c66e97777e4a1909b012ddad92
James DeFelice <james@mesosphere.io>
2017-06-21 21:30:48 +0000

    proto: rebase to latest CSI spec; use gogoslick for protoc

--- proto.go
+++ proto.go
@@ -2,4 +2,4 @@ package csilvm
 
 //go:generate go run gen.go
 //go:generate docker build -t csilvm-proto -f Dockerfile.protobuf3 .
-//go:generate docker run --volume $PWD:/work csilvm-proto protoc --go_out=. csi.proto
+//go:generate docker run --volume $PWD:/work csilvm-proto protoc --gogoslick_out=. csi.proto
