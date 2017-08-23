5d40ff7cb8e17b7815101cabdf9b8a5ab6407c74
Gustav Paul <gpaul@mesosphere.io>
2017-08-23 20:44:09 +0200

    lvm2app: list volume group names and uuids

--- lvm/lvm2app/lvm2app_test.go
+++ lvm/lvm2app/lvm2app_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 )
 
+var _ = fmt.Sprintf
+
 func TestLibraryGetVersion(t *testing.T) {
 	cmd := exec.Command("lvm", "version")
 	out, err := cmd.Output()
@@ -16,7 +18,6 @@ func TestLibraryGetVersion(t *testing.T) {
 	}
 	version := ""
 	for _, line := range strings.Split(string(out), "\n") {
-		fmt.Println(line)
 		line = strings.TrimSpace(line)
 		fields := strings.Split(line, ":")
 		if len(fields) != 2 {
@@ -43,4 +44,63 @@ func TestInit(t *testing.T) {
 		t.Fatal(err)
 	}
 	handle.Close()
+}
+
+func TestValidateVolumeGroupName(t *testing.T) {
+	handle, err := Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer handle.Close()
+	err = ValidateVolumeGroupName(handle, "some bad name ^^^ // ")
+	if err == nil {
+		t.Fatalf("Expected validation to fail")
+	}
+	err = ValidateVolumeGroupName(handle, "simplename")
+	if err != nil {
+		t.Fatalf("Expected validation to succeed but got '%v'", err.Error())
+	}
+}
+
+func TestErr(t *testing.T) {
+	handle, err := Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer handle.Close()
+	exp := `lvm: ValidateVolumeGroupName: Name contains invalid character, valid set includes: [a-zA-Z0-9.-_+]. New volume group name "bad ^^^" is invalid. (-1)`
+	err = ValidateVolumeGroupName(handle, "bad ^^^")
+	if err.Error() != exp {
+		t.Fatalf("Expected '%s' but got '%s'", exp, err.Error())
+	}
+}
+
+func TestListVolumeGroupNames(t *testing.T) {
+	handle, err := Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer handle.Close()
+	names, err := ListVolumeGroupNames(handle)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//TODO(gpaul): this test should create a new volume group then
+	// confirm that its name is contained in the results.
+	fmt.Println(names)
+}
+
+func TestListVolumeGroupUUIDs(t *testing.T) {
+	handle, err := Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer handle.Close()
+	uuids, err := ListVolumeGroupUUIDs(handle)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//TODO(gpaul): this test should create a new volume group then
+	// confirm that its UUID is contained in the results.
+	fmt.Println(uuids)
 }
