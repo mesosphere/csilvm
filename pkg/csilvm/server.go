d624a2c3b17e9f966b690f7195c806e45a215246
Gustav Paul <gpaul@mesosphere.io>
2019-01-07 16:15:13 +0200

    pkg/csilvm: fix import

--- pkg/csilvm/server.go
+++ pkg/csilvm/server.go
@@ -17,6 +17,7 @@ import (
 	"github.com/mesosphere/csilvm/pkg/lvm"
 	"github.com/mesosphere/csilvm/pkg/version"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
